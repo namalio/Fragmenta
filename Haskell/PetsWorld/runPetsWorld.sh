296b52118786b3e022d2f62080e43c5de4c6648c
Nuno Amálio <nuno.amalio@gmail.com>
2025-06-20 14:21:34 +0100

    Fragmenta/PetsWorld

---
+++ Haskell/PetsWorld/runPetsWorld.sh
@@ -0,0 +1,22 @@
+cd ..
+runhaskell PetsWorld/PetsWorld.hs -g
+cd PetsWorld/img
+dot -Tpdf F_AH.gv -o F_AH.pdf
+dot -Tpdf F_PW1.gv -o F_PW1.pdf
+dot -Tpdf F_PW2.gv -o F_PW2.pdf
+dot -Tpdf F_PW3.gv -o F_PW3.pdf
+dot -Tpdf F_PW4.gv -o F_PW4.pdf
+dot -Tpdf F_PW5.gv -o F_PW5.pdf
+dot -Tpdf F_PW6.gv -o F_PW6.pdf
+dot -Tpdf F_PW7.gv -o F_PW7.pdf
+dot -Tpdf F_PW8.gv -o F_PW8.pdf
+dot -Tpdf M_AHW_gfg.gv -o M_AHW_gfg.pdf
+dot -Tpdf M_AHW_Mdl.gv -o M_AHW_Mdl.pdf
+dot -Tpdf M_AHW_uf.gv -o M_AHW_uf.pdf
+dot -Tpdf M_AHW_rf.gv -o M_AHW_rf.pdf
+dot -Tpdf M_PW_gfg.gv -o M_PW_gfg.pdf
+dot -Tpdf M_PW_Mdl.gv -o M_PW_Mdl.pdf
+dot -Tpdf M_PW_uf.gv -o M_PW_uf.pdf
+dot -Tpdf M_PW_rf.gv -o M_PW_rf.pdf
+dot -Tpdf PetRoomWorld_I1.gv -o PetRoomWorld_I1.pdf
+dot -Tpdf PetRoomWorld_I2.gv -o PetRoomWorld_I2.pdf
